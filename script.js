5fdc262831cbe615214ec6806598147578cd8f57
Wesley Schlager <36183540+enragedsnorlax@users.noreply.github.com>
2025-05-02 11:05:15 -0400

    First commit

--- script.js
+++ script.js
@@ -2,93 +2,93 @@
 /* Memory Game */
 /* script.js */
 
-// Select all cards and the restart button
-const cards = document.querySelectorAll('.card'); // nodeList of all cards
-const restartButton = document.querySelector('.restart'); // restart button
- 
-// Game state variables
+
+const cards = document.querySelectorAll('.card'); 
+const restartButton = document.querySelector('.restart'); 
+
+
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard = null;
 let secondCard = null;
 
-// Function to flip 2 cards
+
 function flipCard() {
-    if (lockBoard || this === firstCard) return; // return(do nothing) if board is locked or clicking same card
+    if (lockBoard || this === firstCard) return; 
 
-    this.classList.add('flip'); //add flip style class for flip effect
+    this.classList.add('flip'); 
 
-    if (!hasFlippedCard) { //if no card has been flipped yet
-        hasFlippedCard = true; // next click will skip this if 
-        firstCard = this; // firstCard stores the current card
+    if (!hasFlippedCard) { 
+        hasFlippedCard = true; 
+        firstCard = this; 
         return; 
     }
 
-    // Second card clicked
-    secondCard = this; // Store second card click
-    checkForMatch(); //Check if firstCard and secondCard data-name match
+   
+    secondCard = this; 
+    checkForMatch(); 
 }
 
-// Function to check if two cards match
+
 function checkForMatch() {
-    const isMatch = firstCard.dataset.name === secondCard.dataset.name; // true if match based on html data-name
-    isMatch ? disableCards() : unflipCards(); // Ternary, if isMatch is true, call disableCards, if false, call unFlipCards
+    const isMatch = firstCard.dataset.name === secondCard.dataset.name; 
+    isMatch ? disableCards() : unflipCards(); 
 }
 
-// Disable matched cards
+
 function disableCards() {
-    firstCard.removeEventListener('click', flipCard); // remove event listener first card
-    secondCard.removeEventListener('click', flipCard); // remove event listener second card
-    resetGameState(); //Reset game state after match firstCard,SecondCard = Null / hasFlippedCard = False
+    firstCard.removeEventListener('click', flipCard); 
+    secondCard.removeEventListener('click', flipCard); 
+    resetGameState(); 
 }
 
-// Unflip unmatched cards
+
 function unflipCards() {
-    lockBoard = true; // Disable clicking more cards
-    setTimeout(() => { //executes code with 1 second delay
-        firstCard.classList.remove('flip'); //remove flip style from first flipped card
-        secondCard.classList.remove('flip'); //remove flip style from second flipped card
-        resetGameState(); // Reset state variables
-    }, 1000); // 1000 milisecond (1 second)
+    lockBoard = true; 
+    setTimeout(() => { 
+        firstCard.classList.remove('flip'); 
+        secondCard.classList.remove('flip'); 
+        resetGameState(); 
+    }, 1000); 
 }
 
-// Reset state variables
+
 function resetGameState() {
-    [hasFlippedCard, lockBoard] = [false, false]; // no flipped cards, lockboard not locked
-    [firstCard, secondCard] = [null, null]; // reset previous cards
+    [hasFlippedCard, lockBoard] = [false, false]; 
+    [firstCard, secondCard] = [null, null]; 
 }
 
-// Shuffle cards
+
 function shuffle() {
     cards.forEach(card => {
-        const randomPos = Math.floor(Math.random() * cards.length); // uses arcane magic to generate random number
-        card.style.order = randomPos; //assign random number to card grid position
+        const randomPos = Math.floor(Math.random() * cards.length); 
+        card.style.order = randomPos; 
     });
 }
 
-// Restart the game
+
 function restartGame() {
-    const transitions = document.querySelectorAll('.transitions'); // select all with class transitions
-    transitions.forEach(transition => { // for each 
-        transition.classList.remove('transitions'); // Remove transitions class (made the card positions visible when restarting)
+    const transitions = document.querySelectorAll('.transitions'); 
+    transitions.forEach(transition => {
+        transition.classList.remove('transitions'); 
     });
-    cards.forEach(card => { // For each card
-        card.classList.remove('flip'); // Remove flip style class
-        card.addEventListener('click', flipCard); // add flipCard on click event listener
+    cards.forEach(card => { 
+        card.classList.remove('flip'); 
+        card.addEventListener('click', flipCard); 
     });
-    shuffle(); //Shuffle all cards
-    resetGameState(); // Reset state variables
+    shuffle(); 
+    resetGameState(); 
 
     setTimeout(() => {
-        transitions.forEach(transition => { // for each  
-        transition.classList.add('transitions'); // re add transition class
+        transitions.forEach(transition => { 
+        transition.classList.add('transitions'); 
     });
-},100) //100ms delay;
-} // End restartGame
+},100) 
+} 
 
-// Initialize the game
+
 (function initGame() {
-    shuffle(); // Shuffle Cards
-    cards.forEach(card => card.addEventListener('click', flipCard)); // Add flipcard click event lisener to all cards
-    restartButton.addEventListener('click', restartGame); // Add restartGame to restartButton on click event
-})(); // (); Automatically executes the function
+    shuffle(); 
+    cards.forEach(card => card.addEventListener('click', flipCard)); 
+    restartButton.addEventListener('click', restartGame); 
+})(); 
